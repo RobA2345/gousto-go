e2020990aa9ddc7dd5b9dea572fa9e18c6964147
RobA2345 <rob_lindley@hotmail.com>
2026-01-11 17:36:28 +0000

    Added more recipes

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
@@ -15,14 +14,8 @@ func main() {
 	// Serve files from the current directory
 	fs := http.FileServer(http.Dir("."))
 
-	log.Printf("Gousto Archive serving on http://localhost%s\n", port)
-	log.Println("Access from iPad using your Mac's IP (e.g., http://192.168.1.XX:8080)")
-
 	// Wrap the file server with a "No-Cache" header
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0")
-		w.Header().Set("Expires", time.Unix(0, 0).Format(http.TimeFormat))
-		w.Header().Set("Pragma", "no-cache")
 		fs.ServeHTTP(w, r)
 	})
 
